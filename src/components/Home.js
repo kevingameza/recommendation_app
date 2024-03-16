81461f0bc8c4a8702de28b678e87f3919cb12e27
DanBeltranuwu <dm.beltran@uniandes.edu.co>
2024-03-16 16:57:17 -0500

    add detail management and home render

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 import '../App.css';
 
 function Home() {
-  const [recommendedSongs, setRecommendedSongs] = useState([]);
+  const [recommendedSongs, setRecommendedSongs] = useState(null);
   const userId = localStorage.getItem('userId'); // Get user ID from local storage
 
   useEffect(() => {
@@ -23,7 +23,7 @@ function Home() {
               throw new Error('Failed to fetch song details');
             }
             const songDetails = await songResponse.json();
-            return { ...recomendation, ...songDetails }; // Combine the recommendation and detailed information
+            return { ...recomendation, title: songDetails.title }; // Combine the recommendation and detailed information
           }));
           setRecommendedSongs(songsWithDetails); // Aquí es donde deberíamos usar la variable correcta
         } catch (error) {
@@ -36,15 +36,18 @@ function Home() {
     }
   }, [userId]); // Re-run useEffect when userId changes
 
+  if (!recommendedSongs) {
+    return <div className="detail-view">Loading...</div>;
+  }
+
   return (
     <div className="home-view">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/2560px-Lastfm_logo.svg.png" alt="Last.fm Logo" className="logo" />
       <h1>Recommended Songs</h1>
       <div className="song-grid">
         {recommendedSongs.map((song) => (
-          <Link to={`/songs/${song.item_id}`} key={song.item_id} className="song-card">
+          <Link to={`/songs/${song.id}`} key={song.id} className="song-card">
             <h3>{song.title}</h3>
-            <p>{song.artist}</p>
           </Link>
         ))}
       </div>
